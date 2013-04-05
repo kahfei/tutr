b946b0f9c64e1544d6293a2e32bf1cc79938612f
kahfei <me@kahfei.com>
2013-04-05 22:47:47 +0800

    action and view for index, new and create

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,22 @@ class StudentsController < ApplicationController
   def index
   	@students = Student.all
   end
+
+  def new 
+  	@student = Student.new
+
+  end
+
+  def create
+  	@student = Student.new(params[:student])
+  	if @student.save
+  		redirect_to @student
+  	else
+  		render :action => "new"
+  	end
+  end
+
+  def show
+  	@student = Student.find(params[:id])
+  end
 end
