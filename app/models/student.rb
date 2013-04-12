5f1177c350528b387229bb5755bf72ee60e7f055
kahfei <me@kahfei.com>
2013-04-12 20:31:03 +0800

    student total_fee is the sum of all subjects fee

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ActiveRecord::Base
   has_many :subjects, :through => :enrolments
   has_many :enrolments
   attr_accessible :total_fee, :name, :number, :school, :subject_ids
+
+  def total_fee
+  	total_fee = self.subjects.collect {|subject| subject.fee}.sum
+  end
 end
