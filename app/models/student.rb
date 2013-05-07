d43126e16bb70fc0b0be4c83abdccf055bb79b36
kahfei <me@kahfei.com>
2013-05-08 06:03:32 +0800

    attr_accessible for telephone

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ActiveRecord::Base
   has_many :subjects, :through => :enrolments
   has_many :enrolments
-  attr_accessible :total_fee, :name, :number, :school, :subject_ids, :email
+  attr_accessible :total_fee, :name, :number, :school, :subject_ids, :email, :telephone
 
   def total_fee
   	total_fee = subjects.collect {|subject| subject.fee}.sum
